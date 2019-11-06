859d4d9fc15b2a5cf359ef71aab6f1174711060e
MBCrobotics <56998737+MBCrobotics@users.noreply.github.com>
2019-11-06 12:42:33 +0800

    Update pixycam-blockly.js

--- pixycam-blockly.js
+++ pixycam-blockly.js
@@ -1,6 +1,6 @@
 +(function(window, webduino) {
     'use strict';
-    window.getUART = function(board, rate) {
-        return new webduino.module.UART(board, rate);
+    window.getpixycam = function(board, rate) {
+        return new webduino.module.pixycam(board, rate);
     };
 }(window, window.webduino));
