879fc42e7cf947fbda6013ea49cee6be9a2a7893
MBCrobotics <56998737+MBCrobotics@users.noreply.github.com>
2019-11-06 12:34:11 +0800

    Update zh-hant.js

--- blockly/msg/blocks/zh-hant.js
+++ blockly/msg/blocks/zh-hant.js
@@ -1,4 +1,4 @@
 // pixycam
-Blockly.Msg.WEBDUINO_pixycam_SETTING = "設定 pixycam baud rate:";
+Blockly.Msg.WEBDUINO_pixycam_SETTING = "啟動 pixycam uart通訊 默認baud rate:";
 Blockly.Msg.WEBDUINO_pixycam_RX = "接收資料";
 Blockly.Msg.WEBDUINO_pixycam_TX = "向pixycam要物件資料";
