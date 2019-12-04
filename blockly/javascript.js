c2b2eb9e8aa026ae011062be148f0a0a8bf50ac4
MBCrobotics <56998737+MBCrobotics@users.noreply.github.com>
2019-12-04 22:48:10 +0800

    Update javascript.js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -7,7 +7,9 @@ Blockly.JavaScript['pixycam_new'] = function(block) {
 };
 
 Blockly.JavaScript['pixycam_x'] = function(block) {
-  var variable_pixycam = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('pixycam'), Blockly.Variables.NAME_TYPE);
+  
+    var variable_pixycam = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('pixycam'), Blockly.Variables.NAME_TYPE);
+    console.log(variable_pixycam);
     var dropdown_dataType = "string";
     var code = variable_pixycam + '.' + dropdown_dataType;
     var PIXY_X=0;
