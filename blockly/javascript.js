40f87e217c4e5a8419219cac98ed7e45b5dd5ed5
MBCrobotics <56998737+MBCrobotics@users.noreply.github.com>
2019-12-04 22:12:39 +0800

    Update javascript.js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -71,25 +71,23 @@ Blockly.JavaScript['pixycam_rx'] = function(block) {
 //https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#d9sohf
 Blockly.JavaScript['pixycam_data'] = function(block) {
     var variable_pixycam = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('pixycam'), Blockly.Variables.NAME_TYPE);
-    var dropdown_dataType = block.getFieldValue('dataType');
-    var code = variable_pixycam + '.' + dropdown_dataType;
+    //var dropdown_dataType = block.getFieldValue('dataType');
+    var code = variable_pixycam + '.' + 'string';
     if(code>=7000){
-        PIXY_B=code%1000;
+        code=code%1000;
     }else if(code>=6000){
-        PIXY_G=code%1000;
+        code=code%1000;
     }else if(code>=5000){
-        PIXY_R=code%1000;
+        code=code%1000;
     }else if(code>=4000){
-        PIXY_H=code%1000;
+        code=code%1000;
     }else if(code>=3000){
-        PIXY_W=code%1000;
+        code=code%1000;
     }else if(code>=2000){
-        PIXY_Y=code%1000;
+        code=code%1000;
     }else if(code>=1000){
-        PIXY_X=code%1000;
+        code=code%1000;
     }
-    code = variable_pixycam + '.rx(';
-    code += "async function () {\n" + statements_rx + "});\n";
-    return code;
+    return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
