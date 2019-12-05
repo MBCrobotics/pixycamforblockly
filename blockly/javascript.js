e18d01db757aeec4de2a383200098eefeb43f86a
MBCrobotics <56998737+MBCrobotics@users.noreply.github.com>
2019-12-05 22:43:02 +0800

    Update javascript.js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -6,7 +6,7 @@ Blockly.JavaScript['pixycam_new'] = function(block) {
     return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
 };
 
-let PIXY_X=0;
+let PIXY_X=999;
 let PIXY_Y=0;
 let PIXY_W=0;
 let PIXY_H=0;
@@ -16,7 +16,7 @@ let PIXY_B=0;
 
 //https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#
 Blockly.JavaScript['pixycam_ansys'] = function(block) {
-   var value_rawdata = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('rawdata'), Blockly.Variables.NAME_TYPE);
+   var value_rawdata = Blockly.JavaScript.valueToCode(block, 'rawdata', Blockly.JavaScript.ORDER_ATOMIC);
     if(value_rawdata>=7000)
          PIXY_X=value_rawdata % 1000;
     else if(value_rawdata>=6000)
@@ -42,21 +42,7 @@ Blockly.JavaScript['pixycam_rawdata'] = function(block) {
   return [code , Blockly.JavaScript.ORDER_NONE];
 };
 
-Blockly.JavaScript['pixycam_x'] = function(block) {
-  
-    var variable_pixycam = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('pixycam'), Blockly.Variables.NAME_TYPE);
-    var code = variable_pixycam + '.' + "string";
-    //console.log('code : ' + code);
-    var data=parseInt(code);
-    //console.log('data : ' + data);
-    //var PIXY_X=0;
-    //if(data>=1000 && data<2000){
-        //PIXY_X=data%1000;
-        //console.log('PIXY_X : ' + PIXY_X);
-    //}
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [PIXY_X , Blockly.JavaScript.ORDER_NONE];
-};
+Blockly.JavaScript['pixycam_x'] = PIXY_X;
 
 Blockly.JavaScript['pixycam_y'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
